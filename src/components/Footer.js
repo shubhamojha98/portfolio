53efad851efa665e8770aa010f54a73937faf604
shubhamojha98 <shubhamojha.ojha098@gmail.com>
2024-09-05 17:05:24 +0530

    Added Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { VscGithub } from "react-icons/vsc";
+import { RxGithubLogo } from "react-icons/rx";
+import { FaSquareGithub } from "react-icons/fa6";
+import { BsLinkedin } from "react-icons/bs";
 
-const Footer=()=>{
-    return(
-        <></>
-    )
-}
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="footer-socials">
+          <a href="https://github.com/shubhamojha98" target="_blank" rel="noopener noreferrer"><RxGithubLogo/></a>
+          <a href="https://github.com/shubhamojha98" target="_blank" rel="noopener noreferrer"><BsLinkedin/></a>
+        </div>
+        <div className="copyright">
+          <small>Shubham Kumar Ojha</small>
+        </div>
+      </footer>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,38 @@
 import React from "react";
-import '../App.css';
-import { FaLinkedin,FaGithub } from "react-icons/fa";
+import "../App.css";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import own_img2 from "../assets/own_img.jpg";
 
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="container head_container">
+          <h5>Hello I am</h5>
+          <h1>Shubham Kumar Ojha</h1>
+          <h5>An Aspiring Software Developer</h5>
+          <div className="cnnt_btn">
+            <a href="" className="btn btn-connect">
+              Let's Connect
+            </a>
+            {/* <a href=""></a> */}
+          </div>
+          <div className="headersocial">
+            <a href="https://www.linkedin.com/in/shubham-kumar-ojha-a16923208/" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin />
+            </a>
+            <a href="https://github.com/shubhamojha98" target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+          </div>
 
-const Header=()=>{
-    return(
-        <>
-        <header>
-            <div  className="container head_container">
-                <h5>Hello I am</h5>
-                <h1>Shubham Kumar Ojha</h1>
-                <h5>An Aspiring Software Developer</h5>
-                <div className="cnnt_btn">
-                    <a href="" className="btn btn-connect">Let's Connect</a>
-                    {/* <a href=""></a> */}
-                </div>
-                <div className="headersocial">
-                    <a href=""><FaLinkedin/></a>
-                    <a href=""><FaGithub/></a>
-                </div>
+          <div className="own-image">
+            <img src={own_img2} alt="Own" />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-            </div>
-            
-        </header>
-        </>
-    )
-}
-
-
-export default Header
+export default Header;
